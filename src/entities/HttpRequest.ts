75e3062a171453f2643ce96a9e504aba47e236d4
josemorista <josemorista@id.uff.br>
2023-02-15 13:47:54 -0300

    refactor: state inside request

--- src/entities/HttpRequest.ts
+++ src/entities/HttpRequest.ts
@@ -5,10 +5,10 @@ export class HttpRequest {
   path: string;
   query: Record<string, string | Array<string> | undefined>;
   headers: Record<string, string>;
-  params: Record<string, string>;
-  state: Record<string, any>;
+  params: Record<string, string>
   body?: any;
   files: Array<HttpRequestFile>;
+  [key: string]: any;
 
   constructor(input: Omit<HttpRequest, 'state' | 'files'>) {
     this.body = input.body;
@@ -17,7 +17,6 @@ export class HttpRequest {
     this.params = input.params;
     this.query = input.query;
     this.url = input.url;
-    this.state = {};
     this.files = [];
   }
 }
